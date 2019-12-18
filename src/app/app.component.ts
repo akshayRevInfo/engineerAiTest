41583fd0804917574fa1f7f5ab33f85782dfae3f
akshayRev <akshay@eiysys.in>
2019-12-18 11:45:48 +0530

    added test with api calling and make modal with row data and close popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements ngOnInit{
   title = 'test';
+  tableRenderData : any[] = [];
+  data : any;
+  APIURL = 'https://hn.algolia.com/api/v1/search_by_date?tags=story';
+
+  constructor(private httpservice : Http,
+              private modalService : NgbModal){ }
+
+ngOnInit(){
+	this.gettableData();
+}
+
+//Api calling here---------------------------------
+  gettableData(){
+	  this.httpservice.get(this.APIURL).subscribe(res=>{
+	  this.tableRenderData = JSON.parse(res && res._body ? res && res._body : {});
+	  })
+  }
+
+  openModal(content,data){
+  	const modelref = this.modalService.open(content);
+  	this.data = data;
+  }
 }
